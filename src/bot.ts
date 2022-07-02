5a543359f652035af39214e82ab7a9db107736b4
Jose Gavilán <josegavilanruiz@gmail.com>
2022-07-02 13:32:58 +0200

    Initial commit

---
+++ src/bot.ts
@@ -0,0 +1,31 @@
+import './env'
+import TelegramBot from 'node-telegram-bot-api';
+import fs from 'fs'
+
+const token = process.env.BOT_TOKEN ?? "tokenVacio"
+
+const bot = new TelegramBot(token, {
+    polling: true,
+  });
+
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id
+
+  bot.sendMessage(chatId, 'Hola, soy Quimibot, ¡y hago un montón de cosas! Entre ellas, puedo hacer las siguientes:\n\n' + 
+  '*Otros:*\n' +
+  '*/covid |* Te digo los diez países con más casos de COVID-19 actualmente', {parse_mode: "Markdown"})
+})
+
+bot.onText(/\/covid/, async (msg) => {
+  const chatId = msg.chat.id
+
+  const today = new Date();
+	const day = String(today.getDate()).padStart(2, '0');
+	const month = String(today.getMonth() + 1).padStart(2, '0');
+	const year = today.getFullYear();
+	
+	const message = fs.readFileSync(process.env.TEST_PATH + "/covid"+day+""+month+""+year+".txt",'utf8')
+
+  bot.sendMessage(chatId, message)
+})
+
