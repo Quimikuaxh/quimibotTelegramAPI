2fe29a86bd4f7a3d63897ce5fc31e7a6ad84eb89
Jose Gavilán <josegavilanruiz@gmail.com>
2022-08-05 19:42:48 +0200

    First version of database connection (not tested yet) bis

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -1,8 +1,18 @@
 import * as pokemonDB from '../database/pokemon';
+import pokemonInfo from "../types/pokemonInfo";
 
 export function getAllPokemon(){
     return pokemonDB.getAllPokemon();
 }
 export function getOnePokemon(){
     return;
+}
+export function createPokemon(pokemon:pokemonInfo){
+    return pokemonDB.createPokemon(pokemon);
+}
+export function editPokemon(id: number, pokemon: pokemonInfo){
+    return pokemonDB.updatePokemon(id, pokemon);
+}
+export function deletePokemon(id: number){
+    return pokemonDB.deletePokemon(id);
 }
